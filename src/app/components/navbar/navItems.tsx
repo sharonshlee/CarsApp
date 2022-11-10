5afc6258e6dd81c14d44d00c26bdea2dfe5d293f
Sharon Lee <sharonshlee@icloud.com>
2022-11-10 14:24:00 -0600

    add burger menu for responsive app

--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import tw from "twin.macro";
+import { slide as Menu } from "react-burger-menu";
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../responsive";
+import menuStyles from "./menuStyles";
 
 const ListContainer = styled.ul`
   ${tw`
@@ -8,8 +12,8 @@ const ListContainer = styled.ul`
     list-none
   `}
 `;
-
-const NavItem = styled.li`
+// menu? === menu is an optional type
+const NavItem = styled.li<{ menu?: any }>`
   ${tw`
     text-sm
     md: text-base
@@ -23,22 +27,56 @@ const NavItem = styled.li`
     ease-in-out
     hover:text-gray-700
   `}
+
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
+        text-white
+        text-xl
+        mb-3
+        focus:text-white
+        hover:text-gray-400
+    `}
+    `}
 `;
 
 export function NavItems() {
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+  if (isMobile)
+    return (
+      <Menu right styles={menuStyles}>
+        <ListContainer>
+          <NavItem menu>
+            <a href="https://www.google.com">Home</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="https://www.google.com">Cars</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="https://www.google.com">Services</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="https://www.google.com">Contact Us</a>
+          </NavItem>
+        </ListContainer>
+      </Menu>
+    );
+
   return (
     <ListContainer>
       <NavItem>
-        <a href="#">Home</a>
+        <a href="https://www.google.com">Home</a>
       </NavItem>
       <NavItem>
-        <a href="#">Cars</a>
+        <a href="https://www.google.com">Cars</a>
       </NavItem>
       <NavItem>
-        <a href="#">Services</a>
+        <a href="https://www.google.com">Services</a>
       </NavItem>
       <NavItem>
-        <a href="#">Contact Us</a>
+        <a href="https://www.google.com">Contact Us</a>
       </NavItem>
     </ListContainer>
   );
