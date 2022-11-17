3b7f615153e0b3ddaf14bea90de62ed1ec8c5168
Sharon Lee <sharonshlee@icloud.com>
2022-11-17 14:46:39 -0600

    fix topSection typos

--- src/app/containers/HomePage/topSection.tsx
+++ src/app/containers/HomePage/topSection.tsx
@@ -6,6 +6,7 @@ import tw from "twin.macro";
 import MclarenCarImg from "../../../assets/images/mclaren-orange-big.png";
 import BlobImg from "../../../assets/images/blob.svg";
 import { SCREENS } from "../../components/responsive";
+// import { Button } from "../../components/button";
 
 const TopSectionContainer = styled.div`
   min-height: 400px;
@@ -27,7 +28,7 @@ const LeftContainer = styled.div`
     w-1/2
     flex
     flex-col
-  `}
+  `};
 `;
 
 const RightContainer = styled.div`
@@ -37,7 +38,7 @@ const RightContainer = styled.div`
     flex-col
     relative
     mt-20
-  `}
+  `};
 `;
 
 const Slogan = styled.h1`
@@ -54,7 +55,7 @@ const Slogan = styled.h1`
     sm:leading-snug
     lg:leading-normal
     xl:leading-relaxed
-  `}
+  `};
 `;
 
 const Description = styled.p`
@@ -66,7 +67,7 @@ const Description = styled.p`
     overflow-hidden
     max-h-12
     text-gray-800
-  `}
+  `};
 `;
 
 const BlobContainer = styled.div`
@@ -77,36 +78,32 @@ const BlobContainer = styled.div`
   top: -9em;
   z-index: -1;
   transform: rotate(-30deg);
-
   img {
     width: 100%;
     height: auto;
     max-height: max-content;
   }
-
   @media (min-width: ${SCREENS.sm}) {
     width: 40em;
     max-height: 10em;
-    right; -9em;
+    right: -9em;
     top: -16em;
-    transform: rotate(-25deg)
-  } ;
-
+    transform: rotate(-25deg);
+  }
   @media (min-width: ${SCREENS.lg}) {
     width: 50em;
     max-height: 30em;
-    right; -7em;
+    right: -7em;
     top: -15em;
-    transform: rotate(-25deg)
-  } ;
-
+    transform: rotate(-30deg);
+  }
   @media (min-width: ${SCREENS.xl}) {
     width: 70em;
     max-height: 30em;
-    right; -15em;
+    right: -15em;
     top: -25em;
-    transform: rotate(-20deg)
-  } ;
+    transform: rotate(-20deg);
+  }
 `;
 
 const StandaloneCar = styled.div`
@@ -115,25 +112,21 @@ const StandaloneCar = styled.div`
   right: -6em;
   top: -5em;
   position: absolute;
-
   img {
     width: auto;
     height: 100%;
     max-width: fit-content;
   }
-
   @media (min-width: ${SCREENS.sm}) {
     height: 16em;
     right: -6em;
     top: -6em;
   }
-
   @media (min-width: ${SCREENS.lg}) {
     height: 21em;
     right: -8em;
     top: -5em;
   }
-
   @media (min-width: ${SCREENS.xl}) {
     height: 30em;
     right: -13em;
@@ -141,16 +134,28 @@ const StandaloneCar = styled.div`
   }
 `;
 
+// const ButtonsContainer = styled.div`
+//   ${tw`
+//     flex
+//     flex-wrap
+//     mt-4
+//   `};
+// `;
+
 export function TopSection() {
   return (
     <TopSectionContainer>
       <LeftContainer>
-        <Slogan>Rent The Best Quality Cars with Us!</Slogan>
+        <Slogan>Rent The Best Qulity Car's With Us</Slogan>
         <Description>
           Always choose the best car from our local stores or order it remotely
-          at the beset price for you and get the best quality cars for as long
-          as you like
+          at the best price for you and get the best quality cars for as long as
+          you like
         </Description>
+        {/* <ButtonsContainer>
+          <Button text="Book Your Ride" />
+          <Button theme="filled" text="Sell Your Car" />
+        </ButtonsContainer> */}
       </LeftContainer>
       <RightContainer>
         <BlobContainer>
