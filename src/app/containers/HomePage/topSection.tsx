5331b686fad7e58003da6a4c21792533ba3a56d5
Sharon Lee <sharonshlee@icloud.com>
2022-11-17 15:19:43 -0600

    add button component for rent and sell

--- src/app/containers/HomePage/topSection.tsx
+++ src/app/containers/HomePage/topSection.tsx
@@ -6,7 +6,7 @@ import tw from "twin.macro";
 import MclarenCarImg from "../../../assets/images/mclaren-orange-big.png";
 import BlobImg from "../../../assets/images/blob.svg";
 import { SCREENS } from "../../components/responsive";
-// import { Button } from "../../components/button";
+import { Button } from "../../components/button";
 
 const TopSectionContainer = styled.div`
   min-height: 400px;
@@ -134,13 +134,13 @@ const StandaloneCar = styled.div`
   }
 `;
 
-// const ButtonsContainer = styled.div`
-//   ${tw`
-//     flex
-//     flex-wrap
-//     mt-4
-//   `};
-// `;
+const ButtonsContainer = styled.div`
+  ${tw`
+    flex
+    flex-wrap
+    mt-4
+  `};
+`;
 
 export function TopSection() {
   return (
@@ -152,10 +152,10 @@ export function TopSection() {
           at the best price for you and get the best quality cars for as long as
           you like
         </Description>
-        {/* <ButtonsContainer>
+        <ButtonsContainer>
           <Button text="Book Your Ride" />
           <Button theme="filled" text="Sell Your Car" />
-        </ButtonsContainer> */}
+        </ButtonsContainer>
       </LeftContainer>
       <RightContainer>
         <BlobContainer>
